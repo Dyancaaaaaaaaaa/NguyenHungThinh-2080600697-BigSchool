78226d1fb865e5a61be16e8c4700218df68fb9e1
ASUS <ASUS@LAPTOP-9TTOMSPC>
2023-03-17 20:58:27 +0700

    Add project files.

---
+++ NguyenHungThinh-2080600697/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using NguyenHungThinh_2080600697.Models;
+using NguyenHungThinh_2080600697.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenHungThinh_2080600697.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CourseController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
